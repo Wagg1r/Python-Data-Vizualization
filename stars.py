eab09263ad235b8e8f6e3c66ca105ab9b03f3df0
Thomas Waggoner <126543097+Wagg1r@users.noreply.github.com>
2024-05-16 17:22:14 -0400

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,22 @@
+import math
+x =int(input("enter a number: "))
+
+for i in range(1,x):
+        if (i == 0):
+            print("*"*i)
+        else:
+            print(math.ceil(math.log(i))*"*")
+
+
+
+
+
+            ## Lab 1
+
+**Thomas**
+
+1. sardines
+2. mayo
+3. ketchup
+
+![Planet9_3840x2160.jpg](attachment:Planet9_3840x2160.jpg)
